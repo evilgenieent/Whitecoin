6b8e47f60f895c2215432b73481133cd2ad669dd
dev0tion <dev0tion@hotmail.com>
2015-01-06 16:54:42 +0100

    Fixed build date
    Fixed build date in RPC Console

--- src/version.cpp
+++ src/version.cpp
@@ -34,9 +34,9 @@ const std::string CLIENT_NAME("Whitecoin");
 #endif
 
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ""
+#    define GIT_COMMIT_ID "$Format:%h$"
 #    define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
