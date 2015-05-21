d1f072390840469ae15cf4da2203dc7e20e23f53
Oizopower <oizopower@oizopower.nl>
2015-05-22 00:08:42 +0200

    remove depricated dnsseed

--- src/net.cpp
+++ src/net.cpp
@@ -1154,8 +1154,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-	{"oizo", "seed.oizopower.nl"},
-    {"seednode", "dnsseed.whitecoinpool.com"}
+	{"oizo", "seed.oizopower.nl"}
 };
 
 void ThreadDNSAddressSeed(void* parg)
